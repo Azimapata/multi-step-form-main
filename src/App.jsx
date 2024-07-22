c8f8014f97836c23a11e0f71b41379d007618e15
Azimapata <apataazeem01@gmail.com>
2024-07-22 12:13:15 -0700

    Multi-Step-Form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Formstep1 from './Component/Formstep1'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Formstep1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Formstep1.jsx
@@ -0,0 +1,15 @@
+
+
+const Formstep1 = () => {
+  return (
+    <div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Formstep1
